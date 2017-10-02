50ae6309d2e08126cd2c5dcf6cf5042a43935a96
bharatchopra <bharatchopra28@gmail.com>
2017-10-03 00:48:55 +0530

    mongodb connected and saved a unique user

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require("./services/passport");
 
 const app = express();
-
+mongoose.connect(keys.mongoURI, {
+	useMongoClient: true,
+});
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
