2178f8d7f96de31f0cde7b1b6e195b8820d9db24
bharatchopra <bharatchopra28@gmail.com>
2017-10-04 00:39:56 +0530

    header setup with redux

---
+++ client/src/actions/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => {
+	axios.get('/api/current_user');
+};
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render() {
 		return (
-			<div>Header</div>
+			<nav>
+			    <div class="nav-wrapper" style={{ paddingLeft: 10 }}>
+			      	<a href="#" class="left brand-logo">Survey</a>
+			      	<ul class="right">
+			        	<li><a>Login With Google</a></li>
+			      	</ul>
+			    </div>
+			</nav>
 		);
 	}
 }
