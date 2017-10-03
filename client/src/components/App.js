46ed3ff65c108e5017f22709206c80d707a87898
bharatchopra <bharatchopra28@gmail.com>
2017-10-04 01:34:09 +0530

    Landing page contructed and little fix for header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
+import Landing from './Landing';
 import { fetchUser } from '../actions';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2> SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
 	componentDidMount() {
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	renderContent = () => {
@@ -17,7 +18,7 @@ class Header extends Component {
 		return (
 			<nav>
 			    <div className="nav-wrapper" style={{ paddingLeft: 10 }}>
-			      	<a href="#" className="left brand-logo">Survey</a>
+			      	<Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Survey</Link>
 			      	<ul className="right">
 			        	{this.renderContent()}
 			      	</ul>
