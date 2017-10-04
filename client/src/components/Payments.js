724596af34b42ab8fa2ba2e544002f3132b5dba8
bharatchopra <bharatchopra28@gmail.com>
2017-10-04 18:44:05 +0530

    handling payments at server side

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+import { handleToken } from '../actions';
 
 class Payments extends Component {
 	render() {
 		return (
 			<StripeCheckout 
 				name='Survey App'
-				description='$5 for 5 email credits'
-				amount={500}
-				token={token => console.log(token)}
+				description='Rs.50 for 5 email credits'
+				amount={5000}
+				currency='INR'
+				token={token => this.props.handleToken(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_KEY}
 			>
 				<button className="btn">
@@ -19,4 +23,4 @@ class Payments extends Component {
 	}
 }
 
-export default Payments;
+export default connect(null, { handleToken })(Payments);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -8,6 +9,7 @@ require('./models/User');
 require("./services/passport");
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cookieSession({
 	maxAge: 30 * 24 * 60 * 60 * 1000, 
 	keys: [keys.cookieKey]
@@ -19,6 +21,7 @@ mongoose.connect(keys.mongoURI, {
 	useMongoClient: true,
 });
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
