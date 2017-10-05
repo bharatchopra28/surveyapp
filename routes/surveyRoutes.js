6eded2f39837cc31dba80f3822c5c6280892e5b4
bharatchopra <bharatchopra28@gmail.com>
2017-10-06 00:59:00 +0530

    survey saved, credits deducted and email template modified

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,7 @@ const requireCredits = require("../middlewares/requireCredits");
 const Survey = mongoose.model("survey");
 
 module.exports = app => {
-	app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
 		const { title, from, subject, body, recipients } = req.body;
 
 		const survey = new Survey({
@@ -22,6 +22,15 @@ module.exports = app => {
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
+		
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };
