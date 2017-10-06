83b8fb8db9934d89631c431020dbcbc109d552be
bharatchopra <bharatchopra28@gmail.com>
2017-10-06 22:33:18 +0530

    survey form completed and emails validation added

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { reduxForm, Field } from 'redux-form';
+import React from "react";
+import _ from "lodash";
+import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+
+import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
+
+const FIELDS = [
+	{ label: "Survey Title", name: "title" },
+	{ label: "Subject Line", name: "subject" },
+	{ label: "Email Body", name: "body" },
+	{ label: "Recipients List", name: "emails" }
+];
 
 class SurveyForm extends React.Component {
+	renderFields() {
+		return _.map(FIELDS, ({ name, label }) => {
+			return (
+				<Field
+					key={name}
+					type="text"
+					component={SurveyField}
+					label={label}
+					name={name}
+				/>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
-				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-					<Field type="text" name="surveyTitle" component="input" />
-					<button type="submit">Submit</button>
+				<form
+					onSubmit={this.props.handleSubmit(values =>
+						console.log(values)
+					)}
+				>
+					{this.renderFields()}
+					<Link to="/surveys" className="red btn-flat white-text">
+						Cancel
+					</Link>
+					<button
+						type="submit"
+						className="teal btn-flat right white-text"
+					>
+						Next
+					</button>
 				</form>
 			</div>
 		);
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	errors.emails = validateEmails(values.emails || "");
+
+	_.map(FIELDS, ({ name }) => {
+		if (!values[name]) {
+			errors[name] = "You must provide a value.";
+		}
+	});
+
+	return errors;
+}
+
 export default reduxForm({
-	form: 'surveyForm'
+	validate,
+	form: "surveyForm"
 })(SurveyForm);
