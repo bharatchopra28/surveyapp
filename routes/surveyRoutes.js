1f178386f95ec4544f6cfdd8b735113593634afa
bharatchopra <bharatchopra28@gmail.com>
2017-10-07 02:52:09 +0530

    localtunnel and datesent userid saved in survey model

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -18,11 +18,13 @@ module.exports = app => {
 			body,
 			recipients: recipients
 				.split(",")
-				.map(email => ({ email: email.trim() }))
+				.map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: Date.now()
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		
+
 		try {
 			await mailer.send();
 			await survey.save();
